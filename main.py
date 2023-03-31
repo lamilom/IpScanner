31109f37c970aa53b7c8ba4094ed77300c813d05
Ramiro Martinez <ramirom@MacBook-Pro-de-Ramiro.local>
2023-03-31 17:03:44 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import socket
+import ipaddress
+
+def scan_ports(target_ip, start_port, end_port):
+    for port in range(start_port, end_port + 1):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.1)
+        address = (str(target_ip), port)
+        result = sock.connect_ex(address)
+        if result == 0:
+            print(f"Port {port}: OPEN")
+        sock.close()
+
+# Ejemplo de uso
+ip_range = ipaddress.ip_network("192.168.1.0/24")
+for ip in ip_range.hosts():
+    print(f"Scanning {ip}")
+    scan_ports(ip, 1, 100)
+
+
+
+
+
